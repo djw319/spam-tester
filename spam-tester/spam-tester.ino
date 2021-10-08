59500bc45e1f4759fbcc68bb932044b7ad78b096
djw319 <64276595+djw319@users.noreply.github.com>
2021-10-08 15:12:33 -0400

    Create spam-tester.ino

---
+++ spam-tester/spam-tester.ino
@@ -0,0 +1,16 @@
+/*DJ Walsh
+ *Measurements and Analysis Term Project
+ *This code collects data from an array of thermocouples to test the temperature distrobution of microwaved spam
+ *Initial Commit: 10-08-2021
+ */
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
