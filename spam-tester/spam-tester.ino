84f04491a87940f24587a1b35839b07a18fa2b75
djw319 <64276595+djw319@users.noreply.github.com>
2021-10-12 12:13:13 -0400

    Thermocouple test

--- spam-tester/spam-tester.ino
+++ spam-tester/spam-tester.ino
@@ -4,6 +4,18 @@
  *Initial Commit: 10-08-2021
  */
 
+//libraries
+#include "max6675.h"
+
+
+//Global Variables
+  //Thermoresistor Setup
+    int thermoDO = 4;
+    int thermoCS = 5;
+    int thermoCLK = 6;
+  //Time Setup
+    unsigned long myTime = 0;
+
 
 void setup() {
   // put your setup code here, to run once:
