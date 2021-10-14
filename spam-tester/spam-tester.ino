dc36abde9686a154cdc7e1c6538e180d68025a8b
djw319 <64276595+djw319@users.noreply.github.com>
2021-10-14 15:25:37 -0500

    add multiple thermocouple support
    Only 2 thermocouples, but proves that DO and CLK can be common

--- spam-tester/spam-tester.ino
+++ spam-tester/spam-tester.ino
@@ -11,18 +11,31 @@
 //Global Variables
   //Thermoresistor Setup
     int thermoDO = 4;
-    int thermoCS = 5;
     int thermoCLK = 6;
+    int SC_TC1 = 5;
+    int SC_TC2 = 7;
+    
   //Time Setup
     unsigned long myTime = 0;
 
+//Thermocouple Initialization
+  MAX6675 TC1(thermoCLK, SC_TC1, thermoDO);
+  MAX6675 TC2(thermoCLK, SC_TC2, thermoDO);
 
 void setup() {
   // put your setup code here, to run once:
+
+  Serial.begin(115200);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.print(TC1.readCelsius());
+  Serial.print(", ");
+  Serial.print(TC2.readCelsius());
+  Serial.print("\n");
+
+  delay(200);
 
 }
